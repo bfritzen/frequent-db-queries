0c7aec3405bd1372fce289d1d9c0a89238dcda74
Bruno Fritzen <bruno.fritzen@gmail.com>
2020-02-17 00:06:25 -0300

    List items. Copy and scroll buttons. Blink transition on copy.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :ref="refApp">
+    <h1>Filter queries</h1>
+    <b-input v-model="filter" class="mb-3" />
+    <b-table
+      class="mb-5"
+      tbody-tr-class="button" 
+      head-variant="dark"
+      striped 
+      hover 
+      :items="items" 
+      :filter="filter" 
+      :fields="fields"
+      @row-clicked="onRowClicked">
+    </b-table>
+
+    <div v-for="item in items" v-bind:key="item.description">
+      <h1 :ref="createRefName(item.description)">{{ item.description }}
+        <b-icon-documents class="border p-1 button" @click="copyQueryToClipboard(createRefName(item.description))"></b-icon-documents>
+        <b-icon-arrow-up class="border p-1 button" @click="scrollMeTo(refApp)"></b-icon-arrow-up>
+      </h1>
+      <pre>{{ item.query }}</pre>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { BIconArrowUp, BIconDocuments } from 'bootstrap-vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BIconArrowUp, BIconDocuments
+  },
+  data() {
+    return {
+      filter: '',
+      fields: ['description'],
+      items: [
+          { description: 'All users', query: 'SELECT * FROM USERS;' },
+          { description: 'Employee salary by id', query: 'SELECT salary FROM EMPLOYEE WHERE ID=?;' },
+          { description: 'Actual date', query: 'SELECT sysdate FROM dual;' },
+          { description: 'Most expensive employee', query: 'SELECT * FROM EMPLOYEE WHERE salary = (SELECT MAX(salary) FROM EMPLOYEE);' },
+          { description: 'Employees in each department', query: 'SELECT department_id, sum(1) FROM EMPLOYEE GROUP BY department_id;' },
+        ],
+      refApp: 'refApp'
+    }
+  },
+  methods: {
+    copyQueryToClipboard(refName) {
+      /* eslint-disable no-debugger */
+      // debugger
+      var element = this.$refs[refName][0].nextSibling
+      element.classList.add('blink')
+      this.$copyText(element.innerText)
+      setTimeout(() => { 
+        element.classList.remove('blink')
+        }, 250)
+    },
+    createRefName(description) {
+      return description.split(' ').join('_')
+    },
+    onRowClicked(item) {
+      this.scrollMeTo(this.createRefName(item.description))
+    },
+    scrollMeTo(refName) {
+      var element = this.$refs[refName]
+      if (element.length)
+        element = element[0]
+      var top = element.offsetTop
+      window.scrollTo(0, top)
+    }
   }
 }
 </script>
@@ -24,5 +82,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.button {
+  cursor: pointer;
+}
+.blink {
+  background-color: gray;
+  -webkit-transition: background-color 250ms linear;
+  -ms-transition: background-color 250ms linear;
+  transition: background-color 250ms linear;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueClipboard from 'vue-clipboard2'
+import VueRouter from 'vue-router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -7,6 +9,14 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+// Install VueClipboard
+Vue.use(VueClipboard)
+
+// Install VueRouter
+Vue.use(VueRouter)
+const router = new VueRouter({
+  // routes // short for `routes: routes`
+})
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
@@ -14,4 +24,5 @@ Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
